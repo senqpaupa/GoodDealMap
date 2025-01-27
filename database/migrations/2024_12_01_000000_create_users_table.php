ebbc794a6d158ddb07e23de2d17b39a0fd97f27f
senqa <semen7498@icloud.com>
2025-01-27 23:37:05 +1000

    nullable -> unique

--- database/migrations/2024_12_01_000000_create_users_table.php
+++ database/migrations/2024_12_01_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('phone')->unique();
+            $table->string('phone')->nullable();
             $table->integer('age')->nullable();
             $table->text('about')->nullable();
             $table->string('avatar')->nullable();
